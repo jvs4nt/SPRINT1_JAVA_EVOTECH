ec22c0e84b2f7c5cea39797d07ca7c4a4ff9cfad
jvs4nt <joaovit342@gmail.com>
2024-10-06 21:25:18 -0300

    inicializacao do projeto

---
+++ src/main/java/fiap/_tdspr/evotech/EvotechApplication.java
@@ -0,0 +1,13 @@
+package fiap._tdspr.evotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvotechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/_tdspr/evotech/EvotechApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap._tdspr.evotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
