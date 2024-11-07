0da66588f096f8e8205b9830070f884b93407987
jvs4nt <joaovit342@gmail.com>
2024-11-07 20:38:33 -0300

    criando links na controller de tratamento

--- src/main/java/fiap/_tdspr/evotech/gateways/controllers/TratamentoController.java
+++ src/main/java/fiap/_tdspr/evotech/gateways/controllers/TratamentoController.java
@@ -3,11 +3,17 @@ package fiap._tdspr.evotech.gateways.controllers;
 import fiap._tdspr.evotech.domains.Tratamento;
 import fiap._tdspr.evotech.usecases.TratamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/tratamentos")
@@ -17,31 +23,48 @@ public class TratamentoController {
     private TratamentoService tratamentoService;
 
     @GetMapping
-    public ResponseEntity<List<Tratamento>> getAllTratamentos() {
-        List<Tratamento> tratamentos = tratamentoService.findAll();
-        return ResponseEntity.ok(tratamentos);
+    public ResponseEntity<CollectionModel<EntityModel<Tratamento>>> getAllTratamentos() {
+        List<EntityModel<Tratamento>> tratamentos = tratamentoService.findAll().stream()
+                .map(tratamento -> EntityModel.of(tratamento,
+                        linkTo(methodOn(TratamentoController.class).getTratamentoById(tratamento.getIdTratamento())).withSelfRel(),
+                        linkTo(methodOn(TratamentoController.class).getAllTratamentos()).withRel("allTratamentos")))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(CollectionModel.of(tratamentos,
+                linkTo(methodOn(TratamentoController.class).getAllTratamentos()).withSelfRel()));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Tratamento> getTratamentoById(@PathVariable String id) {
+    public ResponseEntity<EntityModel<Tratamento>> getTratamentoById(@PathVariable String id) {
         Optional<Tratamento> tratamento = tratamentoService.findById(id);
-        return tratamento.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+        if (tratamento.isPresent()) {
+            EntityModel<Tratamento> tratamentoModel = EntityModel.of(tratamento.get(),
+                    linkTo(methodOn(TratamentoController.class).getTratamentoById(id)).withSelfRel(),
+                    linkTo(methodOn(TratamentoController.class).getAllTratamentos()).withRel("allTratamentos"));
+            return ResponseEntity.ok(tratamentoModel);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public ResponseEntity<Tratamento> createTratamento(@RequestBody Tratamento tratamento) {
+    public ResponseEntity<EntityModel<Tratamento>> createTratamento(@RequestBody Tratamento tratamento) {
         Tratamento createdTratamento = tratamentoService.save(tratamento);
-        return ResponseEntity.status(201).body(createdTratamento);
+        EntityModel<Tratamento> tratamentoModel = EntityModel.of(createdTratamento,
+                linkTo(methodOn(TratamentoController.class).getTratamentoById(createdTratamento.getIdTratamento())).withSelfRel(),
+                linkTo(methodOn(TratamentoController.class).getAllTratamentos()).withRel("allTratamentos"));
+        return ResponseEntity.status(201).body(tratamentoModel);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Tratamento> updateTratamento(@PathVariable String id, @RequestBody Tratamento tratamento) {
+    public ResponseEntity<EntityModel<Tratamento>> updateTratamento(@PathVariable String id, @RequestBody Tratamento tratamento) {
         Optional<Tratamento> existingTratamento = tratamentoService.findById(id);
         if (existingTratamento.isPresent()) {
             tratamento.setIdTratamento(id);
             Tratamento updatedTratamento = tratamentoService.save(tratamento);
-            return ResponseEntity.ok(updatedTratamento);
+            EntityModel<Tratamento> tratamentoModel = EntityModel.of(updatedTratamento,
+                    linkTo(methodOn(TratamentoController.class).getTratamentoById(id)).withSelfRel(),
+                    linkTo(methodOn(TratamentoController.class).getAllTratamentos()).withRel("allTratamentos"));
+            return ResponseEntity.ok(tratamentoModel);
         }
         return ResponseEntity.notFound().build();
     }
